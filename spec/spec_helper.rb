6c4fc2c8588f927a08d055686584129d66f461c9
Guinsly Mondésir <guinslym@gmail.com>
2014-07-25 06:27:19 -0400

    Adding factory_girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,9 +12,12 @@
 #
 # The `.rspec` file also contains a few flags that are not defaults but that
 # users commonly want.
-#
+require 'factory_girl'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+config.include FactoryGirl::Syntax::Methods
+  
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
