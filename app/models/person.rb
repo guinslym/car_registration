96c98ecd186ca406ea0f4091653b5b3cecdb63fc
Guinsly Mondésir <guinslym@gmail.com>
2014-07-25 06:25:22 -0400

    rspec for model Person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,36 @@
 class Person < ActiveRecord::Base
+  PROVINCE = ["Ontario", "Quebec", "Nova Scotia", "New Brunswick", 
+              "Manitoba", "British Columbia", "Prince Edward Island", 
+              "Saskatchewan", "Alberta", "Newfoundland and Labrador"]
+  #first name is not mandatory in the conditions but I did it anyway            
+  validates :first_name,:presence => true
+
+  validates :postal_code, format: { with: /\A[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}\z/, message: "Only letters and numbers allowed V4M 3K1" }
+  validates :postal_code, :presence => true
+
+  validates :province, inclusion: { in: PROVINCE,    message: "%{value} is not a valid province" }
+  validates :province, :presence => true
+
+  validates :telephone_number, :presence => true
+  #I used a gem instead of regex
+  validates :telephone_number, :phone_number => {:ten_digits => true, :message => "invalid and can only be attributable to human error"}
+
+
+   #validates_associated :user
+
+  private
+
 end
+
+
+=begin
+  
+(1) first name
+(2) last name
+(3) street address
+(4) city
+(5) province  (from a drop down listing)
+(6) postal code (verify the format is correct)
+(7) telephone number (verify the value is a 10 digit telephone number)
+  
+=end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
+
 RSpec.describe Person, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    context ".province" do
+      it "should be within a range of value" do
+        expect((Person.create(:province => "Ontario", :first_name => "Antoine", :postal_code => "V4M 4L9", :telephone_number => "514-908-9999")).valid?).to be_truthy #be_truthy
+      end
+      it "have error when it's outside a range" do
+        expect(Person.create(:province => "Foo Var", :first_name => "Antoine", :postal_code => "V4M 4L9", :telephone_number => "514-908-9999")).to have(1).error_on(:province)
+      end
+    end
+    it ".postal_code expected to be in a valid format" do
+      expect((Person.create(:telephone_number => "514-908-9999", :province => "Quebec", :first_name => "Antoine", :postal_code => "V4M 4L9")).valid?).to be_truthy
+      expect((Person.create(:telephone_number => "1-514-908-9999", :province => "Quebec", :first_name => "Antoine", :postal_code => "V4MT4L9")).valid?).to be_falsey
+    end
+    it ".telephone_number should be a 10 digit numbers" do
+      expect((Person.create(:province => "Quebec", :first_name => "Antoine", :postal_code => "V4M 4L9", :telephone_number => "514-908-9999")).valid?).to be_truthy
+      expect((Person.create(:province => "Quebec", :first_name => "Antoine", :postal_code => "V4M 4L9", :telephone_number => "5-514-908-9999")).valid?).to be_falsey
+    end
+    it ".first_name is expected to be present" do
+      expect((Person.create(:province => "Quebec", :postal_code => "V4M 4L9", :telephone_number => "5-514-908-9999")).valid?).to be_falsey
+    end
+    it "should have only one user" do
+
+    end
+
 end
+     #expect(subject).to have(1).error_on(:first_name)
+     #expect(subject.valid?).to be_falsey
+=begin
+  
+(1) first name
+(2) last name
+(3) street address
+(4) city
+(5) province  (from a drop down listing)
+(6) postal code (verify the format is correct)
+(7) telephone number (verify the value is a 10 digit telephone number)
+  
+=end
